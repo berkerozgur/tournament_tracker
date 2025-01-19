fb6a67b58670916e621d3eb45122fe5c5e846d9e
Berker Ozgur <89974136+berkerozgur@users.noreply.github.com>
2025-01-19 18:43:55 +0300

    Refactor TournamentViewer layout to use Row and Column for better structure; add ScoreField and BorderedListView components for improved functionality.

--- lib/views/tournament_viewer.dart
+++ lib/views/tournament_viewer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TournamentViewer extends StatefulWidget {
   const TournamentViewer({super.key});
@@ -8,7 +9,6 @@ class TournamentViewer extends StatefulWidget {
 }
 
 class _TournamentViewerState extends State<TournamentViewer> {
-  final bool _isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,43 +18,102 @@ class _TournamentViewerState extends State<TournamentViewer> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
           children: [
-            const DropdownMenu(
-              dropdownMenuEntries: [
-                DropdownMenuEntry<String>(
-                  value: 'foo',
-                  label: 'foo',
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DropdownMenu(
+                  dropdownMenuEntries: [
+                    DropdownMenuEntry<String>(
+                      value: 'foo',
+                      label: 'foo',
+                    ),
+                  ],
+                  label: Text('Round'),
+                  width: 136.6,
                 ),
+                SizedBox(height: 13.6),
+                LabeledCheckbox(label: 'Unplayed only'),
+                SizedBox(height: 13.6),
+                Expanded(child: BorderedListView()),
               ],
-              label: Text('Round'),
-              width: 136.6,
             ),
-            const SizedBox(height: 13.6),
-            const LabeledCheckbox(label: 'Unplayed only'),
-            const SizedBox(height: 13.6),
             Expanded(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(4),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    width: 136.6 * 2,
+                    child: ScoreField(label: 'Team one score'),
                   ),
-                  child: ListView.builder(
-                    itemCount: 1,
-                    itemBuilder: (context, index) {
-                      return const ListTile(
-                        title: Text('title'),
-                        subtitle: Text('subtitle'),
-                      );
-                    },
+                  const SizedBox(height: 13.6),
+                  const SizedBox(
+                    width: 136.6 * 2,
+                    child: ScoreField(label: 'Team two score'),
                   ),
-                ),
+                  const SizedBox(height: 13.6),
+                  SizedBox(
+                    width: 136.6,
+                    child: FilledButton(
+                      onPressed: () {},
+                      child: const Text('Score'),
+                    ),
+                  ),
+                ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScoreField extends StatelessWidget {
+  final String label;
+
+  const ScoreField({
+    super.key,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        label: Text(label),
+        border: const OutlineInputBorder(),
+      ),
+      inputFormatters: [
+        FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+      ],
+      keyboardType: TextInputType.number,
+    );
+  }
+}
+
+class BorderedListView extends StatelessWidget {
+  const BorderedListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.3,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: ListView.builder(
+          itemCount: 1,
+          itemBuilder: (context, index) {
+            return const ListTile(
+              title: Text('title'),
+              subtitle: Text('subtitle'),
+            );
+          },
         ),
       ),
     );
